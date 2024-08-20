46a624a80f57927ea6a34884983f5831ab863383
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2024-08-20 21:13:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+   const romanSymbols = [
+        ['M', 1000],  // 1000
+        ['CM', 900],  // 900 (1000 - 100)
+        ['D', 500],   // 500
+        ['CD', 400],  // 400 (500 - 100)
+        ['C', 100],   // 100
+        ['XC', 90],   // 90 (100 - 10)
+        ['L', 50],    // 50
+        ['XL', 40],   // 40 (50 - 10)
+        ['X', 10],    // 10
+        ['IX', 9],    // 9 (10 - 1)
+        ['V', 5],     // 5
+        ['IV', 4],    // 4 (5 - 1)
+        ['I', 1]      // 1
+    ];
 
-  let result = '';
+    let result = '';
 
+    // Iterate over each Roman symbol
     for (let [symbol, value] of romanSymbols) {
+        // While the current number is greater than or equal to the value
         while (num >= value) {
-            result += symbol;
-            num -= value;
+            result += symbol;  // Append the symbol to the result string
+            num -= value;      // Subtract the value from the number
         }
     }
 
